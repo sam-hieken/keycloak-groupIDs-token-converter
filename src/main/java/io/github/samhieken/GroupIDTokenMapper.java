28ff7ddddd6c0d11b318b0ba4948f1b29d4dd3f7
Sam Hieken <sam.hieken@gmail.com>
2025-09-21 17:11:34 -0400

    Use keycloak code as base

--- src/main/java/io/github/samhieken/GroupIDTokenMapper.java
+++ src/main/java/io/github/samhieken/GroupIDTokenMapper.java
@@ -1,132 +1,103 @@
 package io.github.samhieken;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.keycloak.models.ClientSessionContext;
-import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
-import org.keycloak.models.UserModel;
 import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.ProtocolMapperUtils;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
 import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.protocol.oidc.mappers.TokenIntrospectionTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
 
-public class GroupIDTokenMapper extends AbstractOIDCProtocolMapper
-		implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+/**
+ * Maps user group IDs.
+ * 
+ * Based on https://github.com/keycloak/keycloak/blob/main/services/src/main/java/org/keycloak/protocol/oidc/mappers/GroupMembershipMapper.java
+ */
+public class GroupIDTokenMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper, TokenIntrospectionTokenMapper {
 
-	public static final String PROVIDER_ID = "group-id-token-mapper";
-	private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
 
-	static {
-		OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
-		OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, GroupIDTokenMapper.class);
-	}
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, GroupIDTokenMapper.class);
+    }
 
-//	public static ProtocolMapperModel create(String name, boolean accessToken, boolean idToken, boolean userInfo) {
-//		final ProtocolMapperModel mapper = new ProtocolMapperModel();
-//		mapper.setName(name);
-//		mapper.setProtocolMapper(PROVIDER_ID);
-//		mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
-//		
-//		final Map<String, String> config = new HashMap<>();
-//		config.put(ProtocolMapperUtils.MULTIVALUED, Boolean.TRUE.toString()); // Set the MULTIVALUED config
-//		
-//		if (accessToken) 
-//			config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
-//		
-//		if (idToken) 
-//			config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ID_TOKEN, "true");
-//		
-//		if (userInfo) 
-//			config.put(OIDCAttributeMapperHelper.INCLUDE_IN_USERINFO, "true");
-//		
-//		mapper.setConfig(config);
-//		return mapper;
-//	}
-	
-	public static ProtocolMapperModel createClaimMapper(String name, boolean accessToken, boolean introspectionEndpoint) {
+    public static final String PROVIDER_ID = "group-id-mapper";
+
+
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "User's Group IDs";
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return TOKEN_MAPPER_CATEGORY;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Map user's group IDs";
+    }
+
+
+    /**
+     * Adds the group membership information to the {@link IDToken#otherClaims}.
+     * @param token
+     * @param mappingModel
+     * @param userSession
+     */
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession) {
+        List<String> groupIDs = userSession.getUser().getGroupsStream()
+        		.map(groups -> groups.getId())
+        		.collect(Collectors.toList());
+
+        // force multivalued as the attribute is not defined for this mapper
+        mappingModel.getConfig().put(ProtocolMapperUtils.MULTIVALUED, "true");
+        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, groupIDs);
+    }
+
+    public static ProtocolMapperModel create(String name,
+                                             String tokenClaimName,
+                                             boolean consentRequired, String consentText,
+                                             boolean accessToken, boolean idToken, boolean introspectionEndpoint) {
         ProtocolMapperModel mapper = new ProtocolMapperModel();
         mapper.setName(name);
         mapper.setProtocolMapper(PROVIDER_ID);
         mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
-        Map<String, String> config = new HashMap<>();
-        if (accessToken) {
-            config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
-        } else {
-            config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "false");
-        }
-        if (introspectionEndpoint) {
-            config.put(OIDCAttributeMapperHelper.INCLUDE_IN_INTROSPECTION, "true");
-        } else {
-            config.put(OIDCAttributeMapperHelper.INCLUDE_IN_INTROSPECTION, "false");
-        }
+        
+        Map<String, String> config = new HashMap<String, String>();
+        config.put(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME, tokenClaimName);
+        
+        if (accessToken)
+        	config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
+        if (idToken)
+        	config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ID_TOKEN, "true");
+        if (introspectionEndpoint)
+        	config.put(OIDCAttributeMapperHelper.INCLUDE_IN_INTROSPECTION, "true");
         mapper.setConfig(config);
+
         return mapper;
     }
-
-	@Override
-	protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
-			KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
-
-		boolean shouldUseLightweightToken = getShouldUseLightweightToken(keycloakSession);
-		boolean includeInAccessToken = shouldUseLightweightToken
-				? OIDCAttributeMapperHelper.includeInLightweightAccessToken(mappingModel)
-				: includeInAccessToken(mappingModel);
-		if (!includeInAccessToken) {
-			return;
-		}
-
-		final UserModel user = userSession.getUser();
-		final List<String> groupIds = user.getGroupsStream().map(group -> group.getId()).collect(Collectors.toList());
-
-		if (groupIds != null) {
-			OIDCAttributeMapperHelper.mapClaim(token, mappingModel, groupIds);
-		}
-	}
-
-	private boolean includeInAccessToken(ProtocolMapperModel mappingModel) {
-		final String includeInAccessToken = mappingModel.getConfig()
-				.get(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN);
-
-		// Backwards compatibility
-		if (includeInAccessToken == null) {
-			return true;
-		}
-
-		return "true".equals(includeInAccessToken);
-	}
-
-	@Override
-	public List<ProviderConfigProperty> getConfigProperties() {
-		return configProperties;
-	}
-
-	@Override
-	public String getHelpText() {
-		return "Adds the user's group IDs to the token.";
-	}
-
-	@Override
-	public String getDisplayCategory() {
-		return "Token mapper";
-	}
-
-	@Override
-	public String getDisplayType() {
-		return "Group IDs Token Mapper";
-	}
-
-	@Override
-	public String getId() {
-		return PROVIDER_ID;
-	}
 }
